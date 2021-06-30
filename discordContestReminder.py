4b8764f6caa7161a1149078e0bb3da36c3152565
asubedy <frexpe@pm.me>
2021-06-30 22:55:51 +0545

    optimised the request made to the api

--- discordContestReminder.py
+++ discordContestReminder.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
-import random
 import os
 from discord.ext import commands
 import discord
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import tz
 import dateutil.parser
-import pytz
-import json
 import requests
-import math
+
 
 load_dotenv()
 """
@@ -31,51 +28,14 @@ bot = commands.Bot(command_prefix='#')
 param_query = f"/?username={USER_NAME}&api_key={C_API}"
 
 
-async def contestDisplayer(ctx, resource, name, image):
-    embedVar = discord.Embed(title=f"Upcoming {name} Contests",
-                             description="These are the list of upcoming contest which are less than 7 days away", color=0x00ff00)
-    parameters = {"limit": 4, "resource": f"{resource}", "order_by": "-start"}
-    response = requests.get(
-        f"https://clist.by:443/api/v2/contest/{param_query}", params=parameters)
-
-    contests = response.json()["objects"]
-    from_zone = tz.tzutc()
-    to_zone = tz.tzlocal()
-    utc_time = datetime.now()
-    # embedVar = discord.Embed(title="Upcoming Contests",
-    #                          description="These are the list of upcoming contest which are less than 7 days away", color=0x00ff00)
-    # print(utc_time)
-    flag = 0
-    for single in contests:
-        event_name = single["event"]
-        link = single["href"]
-        duration = int(single['duration'])
-        duration = round(duration/(60*60), 1)
-        starts = dateutil.parser.parse(single['start'])
-        utc_start_time = starts.replace(tzinfo=from_zone)
-        nepal_start_time = utc_start_time.astimezone(to_zone)
-
-        if (nepal_start_time.date()-utc_time.date()).days < 7 and (nepal_start_time.date()-utc_time.date()).days >= 0:
-            flag = 1
-            embedVar.set_thumbnail(
-                url=f"https://clist.by/imagefit/static_resize/64x64/img/resources/{image}.png")
-            embedVar.add_field(name="Contest Name: ",
-                               value=f"{event_name}", inline=False)
-            embedVar.add_field(
-                name="Timings: ", value=f"Date: {nepal_start_time.date()} ({(nepal_start_time.date()-utc_time.date()).days} day left)\nStart Time: {nepal_start_time.time()}\nDuration: {duration} hours", inline=False)
-            embedVar.add_field(name="Registration link: ",
-                               value=f"{link}", inline=False)
-            embedVar.set_footer(
-                text="All timings are in local time zone")
-    if flag == 1:
-        await ctx.send(embed=embedVar)
-        print(ctx.author)
-
-
 async def contestLongDisplayer(ctx, resource, name, image):
+    nowTime = datetime.now().replace(microsecond=0)
+    td = timedelta(10)
+    futureTime = (nowTime+td).isoformat()
     embedVar = discord.Embed(title=f"Upcoming {name} Contests",
                              description="These are the list of upcoming contest which are less than 7 days away", color=0x00ff00)
-    parameters = {"resource": f"{resource}", "order_by": "-start"}
+    parameters = {"resource": f"{resource}", "order_by": "-start",
+                  "start_gte": f"{nowTime}", "start_lte": f"{futureTime}"}
     response = requests.get(
         f"https://clist.by:443/api/v2/contest/{param_query}", params=parameters)
 
@@ -83,9 +43,6 @@ async def contestLongDisplayer(ctx, resource, name, image):
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
     utc_time = datetime.now()
-    # embedVar = discord.Embed(title="Upcoming Contests",
-    #                          description="These are the list of upcoming contest which are less than 7 days away", color=0x00ff00)
-    # print(utc_time)
     flag = 0
     for single in contests:
         event_name = single["event"]
@@ -117,48 +74,11 @@ async def contestLongDisplayer(ctx, resource, name, image):
 
 
 @bot.command(name='upcoming')
-async def leetcodeContest(ctx):
-    # embedVar = discord.Embed(title="Upcoming Leetcode Contests",
-    #                          description="These are the list of upcoming contest which are less than 7 days away", color=0x00ff00)
-    await contestDisplayer(ctx, "atcoder.jp", "AtCcoder", "atcoder_jp")
-    await contestDisplayer(ctx, "leetcode.com", "Leetcode", "leetcode_com")
+async def upcomingContest(ctx):
+    await contestLongDisplayer(ctx, "atcoder.jp", "AtCcoder", "atcoder_jp")
+    await contestLongDisplayer(ctx, "leetcode.com", "Leetcode", "leetcode_com")
     await contestLongDisplayer(ctx, "codeforces.com", "CodeForces", "codeforces_com")
     await contestLongDisplayer(ctx, "codechef.com", "CodeChef", "codechef_com")
-
-    # await contestDisplayer(ctx, "leetcode.com", "leetcode_com")
-
-    # parameters = {"limit": 4, "resource": "leetcode.com", "order_by": "-start"}
-    # response = requests.get(
-    #     f"https://clist.by:443/api/v2/contest/{param_query}", params=parameters)
-
-    # contests = response.json()["objects"]
-    # from_zone = tz.tzutc()
-    # to_zone = tz.tzlocal()
-    # utc_time = datetime.now()
-
-    # # print(utc_time)
-    # for single in contests:
-    #     event_name = "Leetcode "+single["event"]
-    #     link = single["href"]
-    #     duration = int(single['duration'])
-    #     duration = duration/(60*60)
-    #     starts = dateutil.parser.parse(single['start'])
-    #     utc_start_time = starts.replace(tzinfo=from_zone)
-    #     nepal_start_time = utc_start_time.astimezone(to_zone)
-    #     if (nepal_start_time.date()-utc_time.date()).days < 7 and (nepal_start_time.date()-utc_time.date()).days >= 0:
-    #         embedVar.set_thumbnail(
-    #             url="https://clist.by/imagefit/static_resize/64x64/img/resources/leetcode_com.png")
-    #         embedVar.add_field(name="Contest Name: ",
-    #                            value=f"{event_name}", inline=False)
-    #         embedVar.add_field(
-    #             name="Timings: ", value=f"Date: {nepal_start_time.date()} ({(nepal_start_time.date()-utc_time.date()).days} day left)\nStart Time: {nepal_start_time.time()}\nDuration: {duration} hours", inline=False)
-    #         embedVar.add_field(name="Registration link: ",
-    #                            value=f"{link}", inline=False)
-    #         embedVar.set_footer(
-    #             text="All timings are in local time zone")
-
-    # await ctx.send(embed=embedVar)
-    # print(ctx.author)
 
 
 @bot.event
